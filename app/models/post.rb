757182e5556e86e0685939a5b1e8319b16ed4cab
tanaka111 <tanaka.naoki7014@gmail.com>
2019-03-05 20:17:32 +0900

    モデル変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
 belongs_to :user
-belongs_to :genre
+belongs_to :genre, optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true
+  # validates :username, presence: true
   has_many :posts, dependent: :destroy
 end
