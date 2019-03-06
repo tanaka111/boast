226cf56820967f87de9609c503c9bc247b7742e6
tanaka111 <tanaka.naoki7014@gmail.com>
2019-03-06 17:44:04 +0900

    first commit

---
+++ app/assets/javascripts/tab.js
@@ -0,0 +1,26 @@
+// $(function(){
+//   $("tab select2").on("click",function(){
+//     $("card").addClass("hidden");
+//     $("card2").removeClass("hidden");
+//   })
+// });
+
+$(function(){
+  $(".select2 ").on("click",function(){
+    $(".card2").addClass("active");
+    $(".card").removeClass("active");
+  });
+});
+
+
+ // $(function(){
+ //   $(".do").on("click",function(){
+ //     $(".my-profile-container-all1").removeClass("active");
+ //     $(".my-profile-container-all2").addClass("active");
+ //     $(".my-profile-purchase__content__data.hidden").addClass("active");
+ //    });
+ //  $(".notice").on("click",function(){
+ //     $(".my-profile-container-all1").addClass("active");
+ //     $(".my-profile-container-all2").removeClass("active");
+ //     $(".my-profile-purchase__content__data.hidden").removeClass("active");
+ //   });
